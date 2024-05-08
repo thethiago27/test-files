89d82adb8cc8daa669b1ff1fc7c12163dd5ad520
thethiago27 <thethiago27@gmail.com>
2024-05-08 15:25:42 -0400

    feat: create project

--- src/upload_service/upload_service.py
+++ src/upload_service/upload_service.py
@@ -1,6 +1,7 @@
 import os
 import json
 from flask import Flask, request
+from flask_swagger_ui import get_swaggerui_blueprint
 import boto3
 
 app = Flask(__name__)
@@ -10,6 +11,18 @@ sqs = boto3.client('sqs')
 
 S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 SQS_QUEUE_URL = os.environ.get('SQS_QUEUE_URL')
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/docs/swagger.yaml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'File Service API'
+    }
+)
+
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 
 @app.route('/upload', methods=['POST'])
